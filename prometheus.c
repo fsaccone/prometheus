9f0577aeee8a99844dde248e999097e3d4360e0c
Francesco Saccone <francesco@francescosaccone.com>
2025-07-14 18:10:28 +0200

    refactor: define die

--- prometheus.c
+++ prometheus.c
@@ -1,6 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+static void die(const char *m, ...);
+
+void
+die(const char *m, ...)
+{
+	va_list va;
+	va_start(va, m);
+	vfprintf(stderr, m, va);
+	putc('\n', stderr);
+	va_end(va);
+	exit(EXIT_FAILURE);
+}
 
 int
 main(int argc, char *argv[])

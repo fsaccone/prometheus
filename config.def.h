53a3ab29132625153bd54b28524914bc6c399699
Francesco Saccone <francesco@francescosaccone.com>
2025-07-15 17:09:55 +0200

    chore: add comments describing pkgsrepodir

--- config.def.h
+++ config.def.h
@@ -1,3 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 
+/* the directory containing the package repository */
 static char *const pkgsrepodir = "/opt/olympus";
+/* check README for more informations about package repositories */

f9157ec1b407089f189994ac197b7836ef7d18e7
Francesco Saccone <francesco@francescosaccone.com>
2025-07-23 16:29:19 +0200

    chore: rewrite comments of all maximum limits

--- config.def.h
+++ config.def.h
@@ -3,12 +3,12 @@
 #define VERSION     "0.1"
 
 /* maximum limit for package resources */
-#define DEPENDS_MAX  20
-#define OUTS_MAX     50
+#define DEPENDS_MAX  20  /* number of depends entries for a package */
+#define OUTS_MAX     50  /* number of outs entries for a package */
 #define PACKAGES_MAX 500 /* number of packages in a repo */
-#define PROGRAM_MAX  20  /* it also applies to package names */
-#define REQUIRES_MAX 10
-#define SOURCES_MAX  100
+#define PROGRAM_MAX  20  /* length of package names and program names */
+#define REQUIRES_MAX 10  /* number of requires entries for a package */
+#define SOURCES_MAX  100 /* number of sources entries for a package */
 
 #define PACKAGE_REPOSITORY "/opt/olympus" /* the package repository */
 #define DEFAULT_PREFIX     "/"            /* the default prefix */

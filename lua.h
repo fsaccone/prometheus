e2765630a03f9e932f5e8653417e1f3e5e7e16cc
Francesco Saccone <francesco@francescosaccone.com>
2025-07-23 17:02:50 +0200

    feat: declare lua_cp, lua_exec and lua_mkdir lua utility functions

---
+++ lua.h
@@ -0,0 +1,13 @@
+/* See LICENSE file for copyright and license details. */
+#ifndef _LUA_H
+#define _LUA_H
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+int lua_cp(lua_State *luas);
+int lua_exec(lua_State *luas);
+int lua_mkdir(lua_State *luas);
+
+#endif

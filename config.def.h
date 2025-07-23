11fc1f947558f03db3c2b7147caf1314b45d5f2d
Francesco Saccone <francesco@francescosaccone.com>
2025-07-23 10:14:03 +0200

    refactor: define maximum limits for package resources

--- config.def.h
+++ config.def.h
@@ -2,6 +2,13 @@
 #define PROJECTNAME "Prometheus"
 #define VERSION     "0.1"
 
+/* maximum limit for package resources */
+#define DEPENDS_MAX  20
+#define OUTS_MAX     50
+#define PROGRAM_MAX  20 /* it also applies to package names */
+#define REQUIRES_MAX 10
+#define SOURCES_MAX  100
+
 /* the directory containing the package repository */
 static char *const pkgsrepodir = "/opt/olympus";
 /* check README for more informations about package repositories */

62076179383a02447166da2e601b2f4dc852a0fc
Francesco Saccone <francesco@francescosaccone.com>
2025-07-24 11:42:17 +0200

    fix: increase connection timeout to 30 seconds

--- prometheus.c
+++ prometheus.c
@@ -553,7 +553,7 @@ fetchfile(const char *url, const char *f)
 	curl_easy_setopt(c, CURLOPT_FOLLOWLOCATION, 1L);
 	curl_easy_setopt(c, CURLOPT_USERAGENT, ua);
 	curl_easy_setopt(c, CURLOPT_TIMEOUT, 0L);
-	curl_easy_setopt(c, CURLOPT_CONNECTTIMEOUT, 10L);
+	curl_easy_setopt(c, CURLOPT_CONNECTTIMEOUT, 30L);
 	curl_easy_setopt(c, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
 	curl_easy_setopt(c, CURLOPT_USE_SSL, CURLUSESSL_ALL);
 	curl_easy_setopt(c, CURLOPT_SSL_VERIFYPEER, 1L);

dae747edf03543f711e1174f8131a2b94d34257f
Francesco Saccone <francesco@francescosaccone.com>
2025-07-25 17:01:22 +0200

    feat: increase PACKAGES_MAX from 500 to 2000

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 #define VERSION      "0.1"
 
 /* maximum limit for package resources */
-#define DEPENDS_MAX  50  /* number of depends entries for a package */
-#define OUTS_MAX     200 /* number of outs entries for a package */
-#define PACKAGES_MAX 500 /* number of packages in a repo */
-#define SOURCES_MAX  100 /* number of sources entries for a package */
+#define DEPENDS_MAX  50   /* number of depends entries for a package */
+#define OUTS_MAX     200  /* number of outs entries for a package */
+#define PACKAGES_MAX 2000 /* number of packages in a repo */
+#define SOURCES_MAX  100  /* number of sources entries for a package */
 
 #define PACKAGE_REPOSITORY "/opt/prometheus" /* the package repository */
 #define DEFAULT_PREFIX     "/"               /* the default prefix */
